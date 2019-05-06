d8aef3c7dc7414ed1d478ad960eca9c1851f839c
Arve Knudsen <arve.knudsen@gmail.com>
2019-05-06 19:49:47 +0200

    Listen to any interface

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,5 @@ fn main() -> Result<(), std::io::Error> {
     let mut app = tide::App::new(());
     app.at("/").get(async move |_| "Hello, world!");
     app.at("/healthz").get(async move |_| ());
-    Ok(app.serve("127.0.0.1:8000")?)
+    Ok(app.serve("0.0.0.1:8000")?)
 }
