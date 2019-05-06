f76a82b3c896a28b0884edd4768a42048cb15270
Arve Knudsen <arve.knudsen@gmail.com>
2019-05-06 19:13:04 +0200

    Add /healthz endpoint

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,6 @@
 fn main() -> Result<(), std::io::Error> {
     let mut app = tide::App::new(());
     app.at("/").get(async move |_| "Hello, world!");
+    app.at("/healthz").get(async move |_| ());
     Ok(app.serve("127.0.0.1:8000")?)
 }
